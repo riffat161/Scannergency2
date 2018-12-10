bfbb8fe1b096b2b804a6fadddf2d8f9791c52fb9
riffat161 <43749292+riffat161@users.noreply.github.com>
2018-12-10 20:56:57 +0600

    First Commit

---
+++ app/src/main/java/com/example/user/scannergency/TakeImage.java
@@ -0,0 +1,13 @@
+package com.example.user.scannergency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TakeImage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_take_image);
+    }
+}
